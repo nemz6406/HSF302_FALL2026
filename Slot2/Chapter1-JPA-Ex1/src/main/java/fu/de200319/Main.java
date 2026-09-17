d5018a22a7deca35f13a6944b1a71fa410123bef
nemz6406 <namhoang6406@gmail.com>
2026-09-18 02:08:01 +0700

    feat(TODO 0.3): Tạo class Main chạy thử hàm save

--- Slot2/Chapter1-JPA-Ex1/src/main/java/fu/de200319/Main.java
+++ Slot2/Chapter1-JPA-Ex1/src/main/java/fu/de200319/Main.java
@@ -1,17 +1,27 @@
 package fu.de200319;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import fu.de200319.dao.EmployeeDAO;
+import fu.de200319.pojo.Employee;
+import fu.de200319.pojo.Gender;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        EmployeeDAO dao = new EmployeeDAO();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("--- Đang tạo mới nhân viên ---");
+        // Tạo 1 object Employee mới (Trạng thái: NEW/TRANSIENT)
+        Employee emp = new Employee("Nguyen Van A", "a@fpt.edu.vn",
+                new BigDecimal("15000000"), Gender.MALE, LocalDate.of(2022, 3, 1));
+
+        // Gọi hàm save
+        dao.save(emp);
+
+        // Kiểm tra xem ID đã được DB tự động sinh ra chưa
+        System.out.println("Đã lưu thành công! Thông tin nhân viên vừa tạo:");
+        System.out.println(emp); // Sẽ gọi hàm toString() trong Employee
+        System.out.println("ID được sinh ra là: " + emp.getId());
     }
 }
--- Slot2/Chapter1-JPA-Ex1/src/main/java/fu/de200319/dao/EmployeeDAO.java
+++ Slot2/Chapter1-JPA-Ex1/src/main/java/fu/de200319/dao/EmployeeDAO.java
@@ -1,4 +1,27 @@
 package fu.de200319.dao;
 
+import fu.de200319.pojo.Employee;
+import jakarta.persistence.*;
+
 public class EmployeeDAO {
+
+    // Lưu ý: Tên "hsf302FU" bắt buộc phải khớp với thẻ <persistence-unit name="hsf302FU"> trong persistence.xml
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("hsf302FU");
+
+    // ---------- CREATE (TODO 0.3) ----------
+    public void save(Employee e) {
+        // Trước dòng này: e đang ở trạng thái NEW/TRANSIENT
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(e); // -> e chuyển sang trạng thái MANAGED, sẽ được chạy lệnh INSERT khi commit
+            em.getTransaction().commit();
+        } catch (RuntimeException ex) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw ex;
+        } finally {
+            em.close(); // Sau khi EntityManager đóng, e chuyển sang trạng thái DETACHED
+        }
+    }
 }
